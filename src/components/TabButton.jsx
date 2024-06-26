e43ffd8b7b5afa43a93462671aa89f5da060c26c
Mohamed Esmat <msmt0452@gmail.com>
2024-06-26 14:55:12 +0300

    first-commit 🚀

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+const TabButton = ({ children, onSelect, isSelected }) => {
+
+  return (
+    <li><button className={isSelected ? 'active' : ''} onClick={onSelect}>{children }</button></li>
+  )
+}
+
+export default TabButton;
